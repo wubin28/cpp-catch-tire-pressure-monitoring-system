dd595f5d153287a0415fb1919939bdd4f1f92e6a
Ben Wu <wubin28@gmail.com>
2014-10-27 22:58:31 +0000

    Refactored the other two tests to be BDD style.

--- Alarm.tests.cpp
+++ Alarm.tests.cpp
@@ -17,36 +17,26 @@ SCENARIO( "the status of alarm after the transducer probing pressure values", "[
         REQUIRE( alarm->isAlarmOn() );
       }
     }
+
+    WHEN("the sensor probes a normal pressure value") {
+      fakeSensor->fakeNextPressurePsiValue(Alarm::LOW_PRESSURE_THRESHOLD);
+      alarm->check();
+
+      THEN("the alarm will be off") {
+        REQUIRE_FALSE( alarm->isAlarmOn() );
+      }
+    }
+
+    WHEN("the sensor probes a normal pressure value after the one out of range") {
+      fakeSensor->fakeNextPressurePsiValue(Alarm::LOW_PRESSURE_THRESHOLD - 1);
+      fakeSensor->fakeNextPressurePsiValueAgain(Alarm::LOW_PRESSURE_THRESHOLD);
+      alarm->check();
+      alarm->check();
+
+      THEN("the alarm will be off") {
+        REQUIRE_FALSE( alarm->isAlarmOn() );
+      }
+    }
   }
-}
-
-TEST_CASE( "the alarm will be off if the pressure value is normal", "[Intention]" )
-{
-  // Arrange
-  FakeSensor *fakeSensor = new FakeSensor;
-  fakeSensor->fakeNextPressurePsiValue(Alarm::LOW_PRESSURE_THRESHOLD);
-  Alarm *alarm = new Alarm(fakeSensor);
-
-  // Act
-  alarm->check();
-
-  // Assert
-  REQUIRE_FALSE( alarm->isAlarmOn() );
-}
-
-TEST_CASE( "the alarm will be off if a normal pressure value comes after an out of range one", "[NewFeature]" )
-{
-  // Arrange
-  FakeSensor *fakeSensor = new FakeSensor;
-  fakeSensor->fakeNextPressurePsiValue(Alarm::LOW_PRESSURE_THRESHOLD - 1);
-  fakeSensor->fakeNextPressurePsiValueAgain(Alarm::LOW_PRESSURE_THRESHOLD);
-  Alarm *alarm = new Alarm(fakeSensor);
-
-  // Act
-  alarm->check();
-  alarm->check();
-
-  // Assert
-  REQUIRE_FALSE( alarm->isAlarmOn() );
 }
 
