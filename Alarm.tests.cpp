20eaf754b6b238e59ceb32be3b95b3b7e9a31ce4
Ben Wu <wubin28@gmail.com>
2014-10-27 15:48:00 +0000

    Added 3 intention TODOs.

--- Alarm.tests.cpp
+++ Alarm.tests.cpp
@@ -14,4 +14,8 @@ TEST_CASE( "Characterization test for method Alarm.isAlarmOn()", "[Characterizat
   REQUIRE( alarm.isAlarmOn() );
 }
 
+// TODO-intention: the alarm will be on if the pressure value is out of range
+// TODO-intention: the alarm will be off if the pressure value is normal
+// TODO-intention: the alarm will still be on if a normal pressure value comes after an out of range one
+
 // TODO-new-feature: the alarm will be off if a normal pressure value comes after an out of range one
