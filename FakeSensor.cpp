2f5f6c0734cb73bbf4040c7228d1268d7f0f6a86
Ben Wu <wubin28@gmail.com>
2014-10-27 20:06:36 +0000

    Fixed Error: Defined method FakeSensor::fakeNextPressurePsiValueAgain().

--- FakeSensor.cpp
+++ FakeSensor.cpp
@@ -4,6 +4,11 @@ void FakeSensor::fakeNextPressurePsiValue(double nextPressure){
   this->nextPressure = nextPressure;
 }
 
+void FakeSensor::fakeNextPressurePsiValueAgain(double nextPressure){
+  this->nextPressureAgain = nextPressure;
+}
+
 double FakeSensor::popNextPressurePsiValue() {
   return this->nextPressure;
 }
+
