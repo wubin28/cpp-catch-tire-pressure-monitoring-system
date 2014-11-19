b6e6e76635d3a6f3b99d65027ea9bf9fed0b25d9
Ben Wu <wubin28@gmail.com>
2014-11-19 09:38:26 +0000

    Made test 'a pressure value out of range should raise the alarm' to pass.

--- AlarmTest.cpp
+++ AlarmTest.cpp
@@ -2,9 +2,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"  // https://github.com/philsquared/Catch
 
-// TODO-working-on: a normal pressure value should not raise the alarm
-TEST_CASE( "a normal pressure value should not raise the alarm", "feature" )
-{
+TEST_CASE("a normal pressure value should not raise the alarm", "feature") {
   // Arrange
   Alarm* alarm = new Alarm(new FakeSensor(Alarm::LOW_PRESSURE_THRESHOLD));
 
@@ -15,6 +13,16 @@ TEST_CASE( "a normal pressure value should not raise the alarm", "feature" )
   REQUIRE( alarm->isAlarmOn() == false );
 }
 
-// TODO: a pressure value out of range should raise the alarm
+TEST_CASE("a pressure value out of range should raise the alarm") {
+  // Arrange
+  Alarm* alarm = new Alarm(new FakeSensor(Alarm::LOW_PRESSURE_THRESHOLD - 1));
+
+  // Act
+  alarm->check();
+
+  // Assert
+  REQUIRE( alarm->isAlarmOn() == true );
+}
+
 // TODO: a normal pressure value after the out of range one should keep the alarm on
 // TODO-new-feature: a normal pressure value after the out of range one should stop the alarm
