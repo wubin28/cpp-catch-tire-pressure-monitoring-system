38a5e96d2c1b835f91045ef6b49ecec1c3293f10
Ben Wu <wubin28@gmail.com>
2014-11-19 10:16:10 +0000

    Refactored: Changed the style of the tests to be BDD.

--- AlarmTest.cpp
+++ AlarmTest.cpp
@@ -2,36 +2,38 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"  // https://github.com/philsquared/Catch
 
-TEST_CASE("a normal pressure value should not raise the alarm", "feature") {
-  // Arrange
-  Alarm* alarm = new Alarm(new FakeSensor(Alarm::LOW_PRESSURE_THRESHOLD));
+SCENARIO("the status of alarm after checking", "feature") {
+  GIVEN("there is an alarm") {
+    Alarm* alarm = NULL;
+    
+    WHEN("a normal pressure value is checked") {
+      alarm = new Alarm(new FakeSensor(Alarm::LOW_PRESSURE_THRESHOLD));
+      alarm->check();
 
-  // Act
-  alarm->check();
+      THEN("do not raise the alarm") {
+        REQUIRE_FALSE(alarm->isAlarmOn());
+      }
+    }
 
-  // Assert
-  REQUIRE( alarm->isAlarmOn() == false );
+    WHEN("a pressure value out of range is checked") {
+      alarm = new Alarm(new FakeSensor(Alarm::LOW_PRESSURE_THRESHOLD - 1));
+      alarm->check();
+
+      THEN("raise the alarm") {
+        REQUIRE(alarm->isAlarmOn());
+      }
+    }
+
+    WHEN("a normal pressure value after the out of range one is checked") {
+      alarm = new Alarm(new FakeSensor(Alarm::HIGH_PRESSURE_THRESHOLD + 1));
+      alarm->check();
+      alarm->setSensor(new FakeSensor(Alarm::HIGH_PRESSURE_THRESHOLD));
+      alarm->check();
+
+      THEN("stop the alarm") {
+        REQUIRE_FALSE(alarm->isAlarmOn());
+      }
+    }
+
+  }
 }
-
-TEST_CASE("a pressure value out of range should raise the alarm") {
-  // Arrange
-  Alarm* alarm = new Alarm(new FakeSensor(Alarm::LOW_PRESSURE_THRESHOLD - 1));
-
-  // Act
-  alarm->check();
-
-  // Assert
-  REQUIRE( alarm->isAlarmOn() == true );
-}
-
-TEST_CASE("a normal pressure value after the out of range one should stop the alarm") {
-  // Arrange, Act
-  Alarm* alarm = new Alarm(new FakeSensor(Alarm::HIGH_PRESSURE_THRESHOLD + 1));
-  alarm->check();
-  alarm->setSensor(new FakeSensor(Alarm::HIGH_PRESSURE_THRESHOLD));
-  alarm->check();
-
-  // Assert
-  REQUIRE( alarm->isAlarmOn() == false );
-}
-
