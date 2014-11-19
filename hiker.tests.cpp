5cae5ebbbf250ce03743cb078e3ce56efbe36a31
Ben Wu <wubin28@gmail.com>
2014-11-19 08:11:49 +0000

    Created exercise Tire Pressure Monitoring System with C++ and Catch.

---
+++ hiker.tests.cpp
@@ -0,0 +1,9 @@
+#include "hiker.hpp"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"  // https://github.com/philsquared/Catch
+
+TEST_CASE( "Life the universe and everything", "[hhgttg]" )
+{
+    // a simple example to start you off
+    REQUIRE( answer() == 42 );
+}
