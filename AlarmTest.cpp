9d0510536577289099dd4ad586ee81587538d091
Ben Wu <wubin28@gmail.com>
2014-11-19 08:44:22 +0000

    Wrote intention code for test 'a normal pressure value should not raise the alarm'.

--- AlarmTest.cpp
+++ AlarmTest.cpp
@@ -1,14 +1,20 @@
-#include "hiker.hpp"
+#include "Alarm.hpp"
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"  // https://github.com/philsquared/Catch
 
-TEST_CASE( "Life the universe and everything", "[hhgttg]" )
+// TODO-working-on: a normal pressure value should not raise the alarm
+TEST_CASE( "a normal pressure value should not raise the alarm", "feature" )
 {
-    // a simple example to start you off
-    REQUIRE( answer() == 42 );
+  // Arrange
+  Alarm* alarm = new Alarm(new FakeSensor(Alarm.LOW_PRESSURE_THRESHOLD));
+
+  // Act
+  alarm->check();
+
+  // Assert
+  REQUIRE( alarm->isAlarmOn() == false );
 }
 
-// TODO: a normal pressure value should not raise the alarm
 // TODO: a pressure value out of range should raise the alarm
 // TODO: a normal pressure value after the out of range one should keep the alarm on
 // TODO-new-feature: a normal pressure value after the out of range one should stop the alarm
